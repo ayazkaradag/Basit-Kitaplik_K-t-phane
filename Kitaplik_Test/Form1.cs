ca6470067b0618feadf67ccd5b606d4eacc2a31a
Ayaz Karadag <ayazkaradagg@outlook.com>
2023-11-29 16:03:41 +0300

    Proje dosyası ekle.

---
+++ Kitaplik_Test/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kitaplik_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        KitapDB vtsinif = new KitapDB();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = vtsinif.Liste();
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            kitap ktpsinif=new kitap();
+            ktpsinif.ADI=textBox1.Text;
+            ktpsinif.YAZAR=textBox2.Text;
+            vtsinif.KitapEk(ktpsinif);
+            MessageBox.Show("Kayıt Başarılı.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kitaplik_Test/KitapDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Security.Cryptography;
+
+namespace Kitaplik_Test
+{
+    internal class KitapDB
+    {
+        OleDbConnection baglanti=new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\Karadag\Desktop\Kitaplikk.mdb");
+        public List<kitap> Liste()
+        {
+            List<kitap> ktp=new List<kitap>();
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("Select * From Kitaplar", baglanti);
+            OleDbDataReader dr=komut.ExecuteReader();
+            while(dr.Read())
+            {
+                kitap kp=new kitap();
+                kp.ID = Convert.ToInt16(dr[0].ToString());
+                kp.ADI = dr[1].ToString();
+                kp.YAZAR = dr[2].ToString();
+
+                ktp.Add(kp);
+            }
+            baglanti.Close();
+            return ktp;
+        }
+        public void KitapEk(kitap kt)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("insert into kitaplar (KitapAd,Yazar) values (@p1,@p2)", baglanti);
+            komut.Parameters.AddWithValue("@p1",kt.ADI);
+            komut.Parameters.AddWithValue("@p2", kt.YAZAR);
+            komut.ExecuteNonQuery();
+            baglanti.Close() ;
+        }
+    }
+}
